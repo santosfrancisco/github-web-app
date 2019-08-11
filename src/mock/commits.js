dc1422d98c2b8b2d72c998657515c109b8844e77
Francisco Santos <yoshi.df@gmail.com>
2019-08-11 16:33:06 -0300

    feat: create mocks data from storybook and tests

---
+++ src/mock/commits.js
@@ -0,0 +1,160 @@
+export default [
+  {
+    sha: '6c13ea200d12191fbfe6acab62a2eaaab5de96dd',
+    node_id: 'MDY6Q29tbWl0MTYyOTMyNTY3OjZjMTNlYTIwMGQxMjE5MWZiZmU2YWNhYjYyYTJlYWFhYjVkZTk2ZGQ=',
+    commit: {
+      author: {
+        name: 'Al Piepho',
+        email: 'alpiepho@yahoo.com',
+        date: '2019-06-30T15:59:53Z'
+      },
+      committer: {
+        name: 'Francisco Santos',
+        email: 'yoshi.df@gmail.com',
+        date: '2019-06-30T15:59:53Z'
+      },
+      message: 'Adding check for a default Google Analytics ID (#11)',
+      tree: {
+        sha: 'c5cd024519050877ec312e24238ff31ba968b0a3',
+        url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/git/trees/c5cd024519050877ec312e24238ff31ba968b0a3'
+      },
+      url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/git/commits/6c13ea200d12191fbfe6acab62a2eaaab5de96dd',
+      comment_count: 0,
+      verification: {
+        verified: false,
+        reason: 'unsigned',
+        signature: null,
+        payload: null
+      }
+    },
+    url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/6c13ea200d12191fbfe6acab62a2eaaab5de96dd',
+    html_url: 'https://github.com/santosfrancisco/gatsby-starter-cv/commit/6c13ea200d12191fbfe6acab62a2eaaab5de96dd',
+    comments_url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/6c13ea200d12191fbfe6acab62a2eaaab5de96dd/comments',
+    author: {
+      login: 'alpiepho',
+      id: 8628212,
+      node_id: 'MDQ6VXNlcjg2MjgyMTI=',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/8628212?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/alpiepho',
+      html_url: 'https://github.com/alpiepho',
+      followers_url: 'https://api.github.com/users/alpiepho/followers',
+      following_url: 'https://api.github.com/users/alpiepho/following{/other_user}',
+      gists_url: 'https://api.github.com/users/alpiepho/gists{/gist_id}',
+      starred_url: 'https://api.github.com/users/alpiepho/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/alpiepho/subscriptions',
+      organizations_url: 'https://api.github.com/users/alpiepho/orgs',
+      repos_url: 'https://api.github.com/users/alpiepho/repos',
+      events_url: 'https://api.github.com/users/alpiepho/events{/privacy}',
+      received_events_url: 'https://api.github.com/users/alpiepho/received_events',
+      type: 'User',
+      site_admin: false
+    },
+    committer: {
+      login: 'santosfrancisco',
+      id: 15852005,
+      node_id: 'MDQ6VXNlcjE1ODUyMDA1',
+      avatar_url: 'https://avatars3.githubusercontent.com/u/15852005?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/santosfrancisco',
+      html_url: 'https://github.com/santosfrancisco',
+      followers_url: 'https://api.github.com/users/santosfrancisco/followers',
+      following_url: 'https://api.github.com/users/santosfrancisco/following{/other_user}',
+      gists_url: 'https://api.github.com/users/santosfrancisco/gists{/gist_id}',
+      starred_url: 'https://api.github.com/users/santosfrancisco/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/santosfrancisco/subscriptions',
+      organizations_url: 'https://api.github.com/users/santosfrancisco/orgs',
+      repos_url: 'https://api.github.com/users/santosfrancisco/repos',
+      events_url: 'https://api.github.com/users/santosfrancisco/events{/privacy}',
+      received_events_url: 'https://api.github.com/users/santosfrancisco/received_events',
+      type: 'User',
+      site_admin: false
+    },
+    parents: [
+      {
+        sha: 'a18d79eb4d6c582642d7195f588abc3269243e13',
+        url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/a18d79eb4d6c582642d7195f588abc3269243e13',
+        html_url: 'https://github.com/santosfrancisco/gatsby-starter-cv/commit/a18d79eb4d6c582642d7195f588abc3269243e13'
+      }
+    ]
+  },
+  {
+    sha: 'a18d79eb4d6c582642d7195f588abc3269243e13',
+    node_id: 'MDY6Q29tbWl0MTYyOTMyNTY3OmExOGQ3OWViNGQ2YzU4MjY0MmQ3MTk1ZjU4OGFiYzMyNjkyNDNlMTM=',
+    commit: {
+      author: {
+        name: 'renovate[bot]',
+        email: 'renovate[bot]@users.noreply.github.com',
+        date: '2019-05-04T22:03:51Z'
+      },
+      committer: {
+        name: 'Francisco Santos',
+        email: 'yoshi.df@gmail.com',
+        date: '2019-05-04T22:03:51Z'
+      },
+      message: 'Pin dependencies (#4)',
+      tree: {
+        sha: '0ec843db714cb3cfeed167ae43cd2ab857e11aca',
+        url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/git/trees/0ec843db714cb3cfeed167ae43cd2ab857e11aca'
+      },
+      url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/git/commits/a18d79eb4d6c582642d7195f588abc3269243e13',
+      comment_count: 0,
+      verification: {
+        verified: false,
+        reason: 'unsigned',
+        signature: null,
+        payload: null
+      }
+    },
+    url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/a18d79eb4d6c582642d7195f588abc3269243e13',
+    html_url: 'https://github.com/santosfrancisco/gatsby-starter-cv/commit/a18d79eb4d6c582642d7195f588abc3269243e13',
+    comments_url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/a18d79eb4d6c582642d7195f588abc3269243e13/comments',
+    author: {
+      login: 'renovate[bot]',
+      id: 29139614,
+      node_id: 'MDM6Qm90MjkxMzk2MTQ=',
+      avatar_url: 'https://avatars1.githubusercontent.com/in/2740?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/renovate%5Bbot%5D',
+      html_url: 'https://github.com/apps/renovate',
+      followers_url: 'https://api.github.com/users/renovate%5Bbot%5D/followers',
+      following_url: 'https://api.github.com/users/renovate%5Bbot%5D/following{/other_user}',
+      gists_url: 'https://api.github.com/users/renovate%5Bbot%5D/gists{/gist_id}',
+      starred_url: 'https://api.github.com/users/renovate%5Bbot%5D/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/renovate%5Bbot%5D/subscriptions',
+      organizations_url: 'https://api.github.com/users/renovate%5Bbot%5D/orgs',
+      repos_url: 'https://api.github.com/users/renovate%5Bbot%5D/repos',
+      events_url: 'https://api.github.com/users/renovate%5Bbot%5D/events{/privacy}',
+      received_events_url: 'https://api.github.com/users/renovate%5Bbot%5D/received_events',
+      type: 'Bot',
+      site_admin: false
+    },
+    committer: {
+      login: 'santosfrancisco',
+      id: 15852005,
+      node_id: 'MDQ6VXNlcjE1ODUyMDA1',
+      avatar_url: 'https://avatars3.githubusercontent.com/u/15852005?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/santosfrancisco',
+      html_url: 'https://github.com/santosfrancisco',
+      followers_url: 'https://api.github.com/users/santosfrancisco/followers',
+      following_url: 'https://api.github.com/users/santosfrancisco/following{/other_user}',
+      gists_url: 'https://api.github.com/users/santosfrancisco/gists{/gist_id}',
+      starred_url: 'https://api.github.com/users/santosfrancisco/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/santosfrancisco/subscriptions',
+      organizations_url: 'https://api.github.com/users/santosfrancisco/orgs',
+      repos_url: 'https://api.github.com/users/santosfrancisco/repos',
+      events_url: 'https://api.github.com/users/santosfrancisco/events{/privacy}',
+      received_events_url: 'https://api.github.com/users/santosfrancisco/received_events',
+      type: 'User',
+      site_admin: false
+    },
+    parents: [
+      {
+        sha: 'f007950d6d6338bc30d59b750de2d0e0b2dbbaef',
+        url: 'https://api.github.com/repos/santosfrancisco/gatsby-starter-cv/commits/f007950d6d6338bc30d59b750de2d0e0b2dbbaef',
+        html_url: 'https://github.com/santosfrancisco/gatsby-starter-cv/commit/f007950d6d6338bc30d59b750de2d0e0b2dbbaef'
+      }
+    ]
+  }
+]
