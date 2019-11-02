211f813e88e995a237241f82a8421db42bf44bef
Francisco Santos <fwfs1985@gmail.com>
2019-11-02 20:26:24 -0300

    test: refactor repositories-actions tests

--- src/redux/actions/repositories-actions.test.js
+++ src/redux/actions/repositories-actions.test.js
@@ -1,50 +1,63 @@
 import thunk from 'redux-thunk'
 import configureStore from 'redux-mock-store'
-import mockAxios from 'jest-mock-axios'
 
 import * as constants from '../constants'
 import * as repositoriesActions from './repositories'
 import repositories from '../../mock/repositories'
 
+import api from '../api'
+const MockAdapter = require('axios-mock-adapter')
+
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
 describe('async repositories actions', () => {
-  describe('getPosts', () => {
-    it('should creates GET_REPOS_SUCCESS when fetching repositories has been done', () => {
-      mockAxios.get.mockImplementationOnce(() =>
-        Promise.resolve({
-          data: repositories
-        })
-      )
+  let mock
 
-      const expectedActions = [
-        { type: constants.GET_REPOS_REQUEST },
-        { type: constants.GET_REPOS_SUCCESS, payload: repositories }
-      ]
-      const store = mockStore({ repositories: [] })
+  beforeEach(() => {
+    mock = new MockAdapter(api)
+  })
 
-      return store.dispatch(repositoriesActions.getRepositories()).then(() => {
+  afterEach(() => {
+    mock.restore()
+  })
+  it('should creates GET_REPOS_SUCCESS when fetching repositories has been done', () => {
+    mock.onGet('/users/user/repos')
+      .reply(200, repositories)
+    const expectedActions = [
+      { type: constants.GET_REPOS_REQUEST },
+      { type: constants.GET_REPOS_SUCCESS, payload: repositories }
+    ]
+    const store = mockStore({ repositories: [] })
+
+    return store.dispatch(repositoriesActions.getRepositories('user'))
+      .then(() => {
         expect(store.getActions()).toEqual(expectedActions)
       })
+  })
+  it('shoud creates GET_REPOS_FAILURE when fetching repositories has fail', () => {
+    mock.onGet('/users/user/repos').reply(500)
+    const expectedActions = [
+      { type: constants.GET_REPOS_REQUEST },
+      { type: constants.GET_REPOS_FAILURE, payload: 'Request failed with status code 500' }
+    ]
+    const store = mockStore({ repositories: [] })
+
+    return store.dispatch(repositoriesActions.getRepositories('user')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
     })
-    it('shoud creates GET_REPOS_FAILURE when fetching repositories has fail', () => {
-      const errorMessage = 'Fetch failed'
-      mockAxios.get.mockImplementationOnce(() =>
-        Promise.reject(
-          new Error(errorMessage)
-        )
-      )
+  })
+  it('shoud creates GET_REPOS_FAILURE when fetching repositories has fail with 404', () => {
+    mock.onGet('/users/user/repos')
+      .reply(404)
+    const expectedActions = [
+      { type: constants.GET_REPOS_REQUEST },
+      { type: constants.GET_REPOS_FAILURE, payload: 'Usuário não encontrado.' }
+    ]
+    const store = mockStore({ repositories: [] })
 
-      const expectedActions = [
-        { type: constants.GET_REPOS_REQUEST },
-        { type: constants.GET_REPOS_FAILURE, payload: errorMessage }
-      ]
-      const store = mockStore({ repositories: [] })
-
-      return store.dispatch(repositoriesActions.getRepositories()).then(() => {
-        expect(store.getActions()).toEqual(expectedActions)
-      })
+    return store.dispatch(repositoriesActions.getRepositories('user')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
     })
   })
 })
