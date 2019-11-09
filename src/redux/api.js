2fc4b0703becebcd3927d2dbf4f904e05cc312b4
Francisco Santos <fwfs1985@gmail.com>
2019-11-09 20:27:33 -0300

    feat:  migrate axios config to use graphql api

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+export const gql = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+  },
+  timeout: 3000
+})
+
 export default axios.create({
   baseURL: 'https://api.github.com/',
   timeout: 3000
